458a2ecb9242599713536558fab2a48639493b11
MaungMyatAung <85542468+MaungMyatAung@users.noreply.github.com>
2025-04-06 13:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the element with id "level"
+let element = document.getElementById("level");
+
+let level = 0;
+
+// Traverse up the DOM tree until there are no more parents
+while (element) {
+  level++;
+  element = element.parentElement;
+}
+
+// Display the level using alert
+alert("The level of the element is: " + level);
+
